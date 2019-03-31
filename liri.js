4e14cb067ef53e4b37bc6b713ee1cdab709dcd0b
Ben Shurts <bshurts93@gmail.com>
2019-03-31 16:37:54 -0500

    Used inquirer to make navigation and user input easier and faster

--- liri.js
+++ liri.js
@@ -21,146 +21,160 @@ for (var i = 3; i < process.argv.length; i++) {
     input.push(process.argv[i]);
 }
 
-// INQUIRER FLOW
-inquirer.prompt([
-    {
-        type: "list",
-        name: "searchType",
-        message: "What would you like to do?",
-        choices: ["Seach concerts", "Search Spotify", "Search movies", "Test app"]
-    }
-]).then(function (answer) {
-    var selection = answer.searchType;
+// INQUIRER BASE PROMPT
+function liriMenu() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "searchType",
+            message: "What would you like to do?",
+            choices: ["Search concerts", "Search Spotify", "Search movies", "Test app"]
+        }
+    ]).then(function (answer) {
+        var selection = answer.searchType;
 
-    switch (selection) {
-        case "Search concerts":
-            console.log("SEARCHING CONCERTS");
-            break;
-        case "Search Spotify":
-            console.log("SEARCHING SPOTIFY");
-            break;
-        case "Search movies":
-            console.log("SEARCHING MOVIES");
-            break;
-        case "Test app":
-            console.log("TESTING APP");
-            break;
-        default:
-            console.log("ERR");
-    }
-});
-
+        switch (selection) {
+            case "Search concerts":
+                concertThis();
+                break;
+            case "Search Spotify":
+                spotifyThisSong();
+                break;
+            case "Search movies":
+                movieThis();
+                break;
+            case "Test app":
+                console.log("TESTING APP");
+                break;
+            default:
+                console.log("ERR");
+        }
+    });
+}
 
 
 
 // API FUNCTIONS
 function concertThis(artist) {
-    if (!artist) { artist = input.join("%20") };
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-    axios.get(queryURL)
-        .then(function (response) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "artist",
+            message: "What artist do you want to see?",
+        }
+    ]).then(function (answer) {
+        artist = answer.artist;
 
-            console.log("\r\n\r\n");
-            console.log("BANDSINTOWN DATA FOR " + artist.toUpperCase());
-            for (var i = 0; i < 5; i++) {
-                var venue = response.data[i].venue.name;
-                var city = response.data[i].venue.city;
-                var region = response.data[i].venue.region;
-                var date = response.data[i].datetime;
+        var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
+        axios.get(queryURL)
+            .then(function (response) {
+
+                console.log("\r\n\r\n");
+                console.log("BANDSINTOWN DATA FOR " + artist.toUpperCase());
+                for (var i = 0; i < 5; i++) {
+                    var venue = response.data[i].venue.name;
+                    var city = response.data[i].venue.city;
+                    var region = response.data[i].venue.region;
+                    var date = response.data[i].datetime;
+
+                    console.log(consoleLine);
+                    console.log("Venue: " + venue);
+                    console.log("City: " + city + ", " + region);
+                    console.log("Date: " + date);
+                }
                 console.log(consoleLine);
-                console.log("Venue: " + venue);
-                console.log("City: " + city + ", " + region);
-                console.log("Date: " + date);
-            }
-            console.log(consoleLine);
-            console.log("\r\n\r\n");
-        });
+                console.log("\r\n\r\n");
+            });
+    });
+
 }
 function spotifyThisSong(song) {
-    if (!song) { song = input.join(" "); }
-
-    spotify.search({ type: 'track', query: song }, function (err, data) {
-        if (err) {
-
-            console.log("\r\n\r\n");
-            console.log("-------------------------------------");
-            console.log("Can't find that song! Here's another.");
-            console.log("Song: The Sign");
-            console.log("Artist: Ace of Base");
-            console.log("Album: Happy Nation");
-            console.log("Preview: not available");
-            console.log("-------------------------------------");
-            console.log("\r\n\r\n");
-
-            return console.log('Error occurred: ' + err);
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "song",
+            message: "What song are you searching for?",
         }
-        console.log("\r\n\r\n");
-        console.log("SPOTIFY DATA FOR " + song.toUpperCase());
+    ]).then(function (answer) {
+        song = answer.song;
 
-        for (var i = 0; i < 3; i++) {
-            var track = data.tracks.items[i].name;
-            var artist = data.tracks.items[i].artists[0].name;
-            var album = data.tracks.items[i].album.name;
-            var link = data.tracks.items[i].preview_url;
+        spotify.search({ type: 'track', query: song }, function (err, data) {
+            if (err) {
 
-            console.log(consoleLine);
-            console.log("Song: " + track);
-            console.log("Artist: " + artist);
-            console.log("Album: " + album);
-            if (link === null) {
+                console.log("\r\n\r\n");
+                console.log("-------------------------------------");
+                console.log("Can't find that song! Here's another.");
+                console.log("Song: The Sign");
+                console.log("Artist: Ace of Base");
+                console.log("Album: Happy Nation");
                 console.log("Preview: not available");
-            } else {
-                console.log("Preview: " + link);
+                console.log("-------------------------------------");
+                console.log("\r\n\r\n");
+
+                return console.log('Error occurred: ' + err);
             }
-        }
-        console.log(consoleLine);
-        console.log("\r\n\r\n");
-    });
-}
-function movieThis(movie) {
-    if (!movie) { movie = input.join("+"); }
-
-    var queryURL = "http://www.omdbapi.com/?apikey=" + keys.omdb.key + "&t=" + movie;
-
-    axios.get(queryURL)
-        .then(function (response) {
-            var movie = response.data;
-
             console.log("\r\n\r\n");
-            console.log("OMDB DATA FOR " + movie.Title.toUpperCase());
-            console.log(consoleLine);
-            console.log("Title: " + movie.Title);
-            console.log("Year: " + movie.Year);
-            console.log("Rating: " + movie.Rated);
-            console.log("Rotten Tomatoes: " + movie.Ratings[1].Value);
-            console.log("Language: " + movie.Language);
-            console.log("Actors: " + movie.Actors);
-            console.log("\r");
-            console.log("Plot: " + movie.Plot);
+            console.log("SPOTIFY DATA FOR " + song.toUpperCase());
+
+            for (var i = 0; i < 3; i++) {
+                var track = data.tracks.items[i].name;
+                var artist = data.tracks.items[i].artists[0].name;
+                var album = data.tracks.items[i].album.name;
+                var link = data.tracks.items[i].preview_url;
+
+                console.log(consoleLine);
+                console.log("Song: " + track);
+                console.log("Artist: " + artist);
+                console.log("Album: " + album);
+                if (link === null) {
+                    console.log("Preview: not available");
+                } else {
+                    console.log("Preview: " + link);
+                }
+            }
             console.log(consoleLine);
             console.log("\r\n\r\n");
         });
-}
+    });
 
-// CONCERT THIS
-if (command === "concert-this") {
-    concertThis();
-}
 
-// SPOTIFY THIS SONG
-if (command === "spotify-this-song") {
-    spotifyThisSong();
 }
+function movieThis(movie) {
 
-// MOVIE THIS
-if (command === "movie-this") {
-    movieThis();
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "movie",
+            message: "What movie would you like to know about?",
+        }
+    ]).then(function (answer) {
+        movie = answer.movie;
+
+        var queryURL = "http://www.omdbapi.com/?apikey=" + keys.omdb.key + "&t=" + movie;
+
+        axios.get(queryURL)
+            .then(function (response) {
+                var movie = response.data;
+
+                console.log("\r\n\r\n");
+                console.log("OMDB DATA FOR " + movie.Title.toUpperCase());
+                console.log(consoleLine);
+                console.log("Title: " + movie.Title);
+                console.log("Year: " + movie.Year);
+                console.log("Rating: " + movie.Rated);
+                console.log("Rotten Tomatoes: " + movie.Ratings[1].Value);
+                console.log("Language: " + movie.Language);
+                console.log("Actors: " + movie.Actors);
+                console.log("\r");
+                console.log("Plot: " + movie.Plot);
+                console.log(consoleLine);
+                console.log("\r\n\r\n");
+            });
+    });
 }
-
-// DO WHAT IT SAYS
-if (command === "do-what-it-says") {
+function testApp() {
     fs.readFile('random.txt', "utf8", function (err, data) {
         if (err) throw err;
         dataArr = data.toString().split(",")
@@ -171,3 +185,39 @@ if (command === "do-what-it-says") {
         movieThis(dataArr[5]);
     });
 }
+
+
+// liriMenu();
+testApp();
+
+
+
+// V1 COMMAND LINE CONTROLS (BASE ASSIGNMENT)
+
+// // CONCERT THIS
+// if (command === "concert-this") {
+//     concertThis();
+// }
+
+// // SPOTIFY THIS SONG
+// if (command === "spotify-this-song") {
+//     spotifyThisSong();
+// }
+
+// // MOVIE THIS
+// if (command === "movie-this") {
+//     movieThis();
+// }
+
+// // DO WHAT IT SAYS
+// if (command === "do-what-it-says") {
+//     fs.readFile('random.txt', "utf8", function (err, data) {
+//         if (err) throw err;
+//         dataArr = data.toString().split(",")
+//         // console.log(dataArr);
+
+//         spotifyThisSong(dataArr[1]);
+//         concertThis(dataArr[3]);
+//         movieThis(dataArr[5]);
+//     });
+// }
